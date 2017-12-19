29c57a46bcbf166f053370c6c44a7e2402c39891
Unknown <pichard.thomas@icloud.com>
2017-12-19 12:58:48 +0100

    Mise à jour noms dossiers

---
+++ src/pages/film/film.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-film',
+  templateUrl: 'film.html'
+})
+export class FilmPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
