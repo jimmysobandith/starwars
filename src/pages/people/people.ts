1b282d46d82f265168d97ce1095c3004d2279714
Jimmy <jimmy.sobandith@outlook.fr>
2018-01-09 16:38:04 +0100

    Ajout des personnages & vehicules

--- src/pages/people/people.ts
+++ src/pages/people/people.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { NavController, NavParams } from 'ionic-angular';
+import { Observable } from 'rxjs/Observable';
+import { HttpClient } from '@angular/common/http';
+ 
 @Component({
   selector: 'page-people',
-  templateUrl: 'people.html'
+  templateUrl: 'people.html',
 })
 export class PeoplePage {
-
-  constructor(public navCtrl: NavController) {
-
+  personnages: Observable<any>;
+ 
+  constructor(public navCtrl: NavController, public navParams: NavParams, private httpClient: HttpClient) { 
+    this.personnages = this.httpClient.get('https://swapi.co/api/people');  
   }
-
 }
--- src/pages/vehicule/vehicule.ts
+++ src/pages/vehicule/vehicule.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { NavController, NavParams } from 'ionic-angular';
+import { Observable } from 'rxjs/Observable';
+import { HttpClient } from '@angular/common/http';
+ 
 @Component({
   selector: 'page-vehicule',
-  templateUrl: 'vehicule.html'
+  templateUrl: 'vehicule.html',
 })
 export class VehiculePage {
-
-  constructor(public navCtrl: NavController) {
-
+  vehicules: Observable<any>;
+ 
+  constructor(public navCtrl: NavController, public navParams: NavParams, private httpClient: HttpClient) { 
+    this.vehicules = this.httpClient.get('https://swapi.co/api/vehicles');  
   }
-
 }
