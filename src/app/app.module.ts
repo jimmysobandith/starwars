200b1b7795c9922d51436d7c5f08a715b62c1f40
Jimmy <jimmy.sobandith@outlook.fr>
2018-01-09 15:39:13 +0100

    Affichage des films

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
@@ -10,6 +11,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { VehiculePage } from '../pages/vehicule/vehicule';
 import { PeoplePage } from '../pages/people/people';
 import { FilmPage } from '../pages/film/film';
+import { FilmDetailsPage } from '../pages/filmDetails/filmDetails';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -19,20 +21,27 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     MyApp,
     FilmPage,
     PeoplePage,
+    AboutPage,
     VehiculePage,
+    ContactPage,
+    FilmDetailsPage,
     HomePage,
     TabsPage
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     FilmPage,
+    AboutPage,
     VehiculePage,
+    ContactPage,
     PeoplePage,
+    FilmDetailsPage,
     HomePage,
     TabsPage
   ],
--- src/pages/film/film.ts
+++ src/pages/film/film.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { NavController, NavParams } from 'ionic-angular';
+import { Observable } from 'rxjs/Observable';
+import { HttpClient } from '@angular/common/http';
+import { FilmDetailsPage } from '../filmDetails/filmDetails';
+ 
 @Component({
   selector: 'page-film',
-  templateUrl: 'film.html'
+  templateUrl: 'film.html',
 })
 export class FilmPage {
-
-  constructor(public navCtrl: NavController) {
-
+  films: Observable<any>;
+ 
+  constructor(public navCtrl: NavController, public navParams: NavParams, private httpClient: HttpClient) { 
+    this.films = this.httpClient.get('https://swapi.co/api/films');  
   }
-
+ 
+  openDetails(film) {
+    this.navCtrl.push(FilmDetailsPage, {film: film});
+  }
 }
